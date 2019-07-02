6b75394bb26ee8e185473fa75e4c2fa89a763128
Darren Li <beckzayi@126.com>
2019-07-02 16:53:55 +1000

    keys sample api connection of Jushuitan

---
+++ erp/manufacturers/kuegou/keys_sample.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Sample API details in Jushuitan ERP (聚水潭)
+ */
+$env = array(
+    'sandbox' => false, //测试环境还是正式环境
+    'debug_mode' => false, //是否输出日志
+    'partner_id' => 'xxxxxxxxxxxxxxxxxxxxxxxxx',
+    'partner_key' => 'yyyyyyyyyyyyyyyyyyyyyyyyyy',
+    'token' => 'zzzzzzzzzzzzzzzzzzzzzzzzzzz'
+);
+
+return $env;
